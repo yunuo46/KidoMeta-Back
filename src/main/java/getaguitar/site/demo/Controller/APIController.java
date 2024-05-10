6183646492eef319843dd56dbc62b14601ababdb
송현우 <83938394+yunuo46@users.noreply.github.com>
2024-05-11 03:35:06 +0900

    fix : move test

--- src/main/java/getaguitar/site/demo/Controller/APIController.java
+++ src/main/java/getaguitar/site/demo/Controller/APIController.java
@@ -3,20 +3,17 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import getaguitar.site.demo.Dto.ResUserDto;
 import getaguitar.site.demo.Service.APIService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET})
 @RequestMapping("/api")
 public class APIController {
 
     private final APIService apiService;
 
     @GetMapping("/user")
-    @CrossOrigin(origins = "*", allowedHeaders = "*")
     public ResUserDto getUser() throws JsonProcessingException {
         return apiService.getUser();
     }
